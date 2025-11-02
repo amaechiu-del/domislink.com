38749f9dc133f13a2cef458252997ddc9ebba51f
amaechiu-source <amaechi.u@gmail.com>
2025-11-02 16:09:21 +0100

    Initial commit - Domislink project setup

--- serve_model.py
+++ serve_model.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def docker_home():
     return "Hello from Docker!"
+
+@app.route("/gpt")
+def gpt_home():
+    return "Hello from GPT-OSS!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
